01a05f4b4f61e080c25a9c08fd94480b98bdf26a
thenightterorx <112033887+thenightterorx@users.noreply.github.com>
2023-02-15 11:25:46 -0500

    push virus

--- seats/seats.js
+++ seats/seats.js
@@ -62,7 +62,7 @@ function shuffleCookie() {
 
   function displayCookie(){
     array=getCookie();
-    //document.getElementById("1").innerHTML = "Nw etext!";
+    //document.getElementById("1").innerHTML = "New etext!";
 
     for (i=1;i<=array.length;i++){
         document.getElementById(String(i)).innerHTML = array[i-1][0]+" "+array[i-1][1];
